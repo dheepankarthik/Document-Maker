f58370e9ecd887f78c28b63ecd22e29c101cd781
Dheepan Karthik A <dheepankarthik2008@gmail.com>
2025-12-15 07:18:48 +0530

    Made some changes to main2.py, need to start Prototypes 2 and 3

--- Prototype 1 (Python)/main2.py
+++ Prototype 1 (Python)/main2.py
@@ -11,22 +11,48 @@ titleLabel = tk.Label(root, text="Welcome to the Document Maker", font=("Times N
 titleLabel.grid(row=0, column=0, sticky="nsew", ipady=20, ipadx=15)
 
 buttonsFrame = tk.Frame(root, width=750)
-buttonsFrame.grid(row=1, column=0, pady=50)
+buttonsFrame.grid(row=1, column=0)
 
-outputFrame = tk.Frame(root, width=900)
-outputFrame.grid(row=1, column=0)
+## Output Frame 
+
+outputFrame = tk.Frame(root)
+outputFrame.grid(row=2, column=0, sticky="nsew")
+
+canvas = tk.Canvas(outputFrame, width=900, height=500)
+scrollbar = tk.Scrollbar(outputFrame, orient="vertical", command=canvas.yview)
+
+canvas.configure(yscrollcommand=scrollbar.set)
+
+canvas.grid(row=0, column=0, sticky="nsew")
+scrollbar.grid(row=0, column=1, sticky="ns")
+
+outputFrame = tk.Frame(canvas)
+canvas.create_window((0, 0), window=outputFrame, anchor="nw")
+
+outputFrame.bind(
+    "<Configure>",
+    lambda e: canvas.configure(scrollregion=canvas.bbox("all"))
+)
+
+## End of Output Frame
+
 
 # Functions
 
-def inputDialog():
-    popup = tk.Toplevel(buttonsFrame)
-    popup.geometry("800x450")
-    popup.title("Input")
+outputComponents = 0
+
+def insertTextBox():
+    global outputFrame, outputComponents
+    textBoxToAppend = tk.Text(outputFrame, width=30)
+    textBoxToAppend.grid(row=outputComponents, column=0)
+    outputComponents = outputComponents + 1
+
+
 
 # Buttons Frame
 
-inputDialogButton = tk.Button(buttonsFrame, text="Input", command=inputDialog, font=("Arial", 16))
-inputDialogButton.grid(row=0, column=0)
+textBoxButton = tk.Button(buttonsFrame, text="Insert a text box", command=insertTextBox)
+textBoxButton.grid(row=0, column=0)
 
 
 #Running the app
