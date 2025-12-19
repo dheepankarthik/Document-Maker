5d2c56c6e592334598e44a3bdbb65f273e3bd9a6
Dheepan Karthik A <dheepankarthik2008@gmail.com>
2025-12-19 21:38:09 +0530

    Title added in main3.py. Initial layout plan added to project.

--- Prototype 1 (Python)/main3.py
+++ Prototype 1 (Python)/main3.py
@@ -1,6 +1,7 @@
 import PySide6
 import sys
-from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QHBoxLayout
+from PySide6.QtCore import Qt
 
 app = QApplication(sys.argv)
 
@@ -10,6 +11,15 @@ window.setWindowTitle("Document Maker")
 
 window.resize(400, 300)
 
+label = QLabel("Welcome to the Document Maker")
+
+layout = QVBoxLayout()
+layout.addWidget(label, alignment=Qt.AlignTop)
+
+window.setLayout(layout)
+
 window.show()
+
+
 
 sys.exit(app.exec())
