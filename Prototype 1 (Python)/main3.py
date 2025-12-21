6eecef2852d568dd78e9dde029a9a51322d21910
Dheepan Karthik A <dheepankarthik2008@gmail.com>
2025-12-21 09:20:05 +0530

    Added layouts in main3.py

--- Prototype 1 (Python)/main3.py
+++ Prototype 1 (Python)/main3.py
@@ -9,14 +9,47 @@ window = QWidget()
 
 window.setWindowTitle("Document Maker")
 
-window.resize(400, 300)
+window.resize(1600, 900)
 
 label = QLabel("Welcome to the Document Maker")
+label.setFixedHeight(75)
 
-layout = QVBoxLayout()
-layout.addWidget(label, alignment=Qt.AlignTop)
+# Layouts
 
-window.setLayout(layout)
+mainLayout = QVBoxLayout()
+
+titleLayout = QVBoxLayout()
+
+titleLayout.addWidget(label, alignment=Qt.AlignTop)
+
+workLayout = QHBoxLayout()
+
+buttonLayout = QVBoxLayout()
+
+workspaceLayout = QVBoxLayout()
+window.setLayout(mainLayout)
+
+mainLayout.addLayout(titleLayout)
+mainLayout.addLayout(workLayout)
+workLayout.addLayout(buttonLayout)
+workLayout.addLayout(workspaceLayout)
+
+buttonLayoutTitle = QLabel("Options")
+workspaceLayoutTitle = QLabel("Workspace")
+
+buttonLayout.addWidget(buttonLayoutTitle, alignment=Qt.AlignTop)
+workspaceLayout.addWidget(workspaceLayoutTitle, alignment=Qt.AlignTop)
+
+#Styling 
+
+app.setStyleSheet("""
+QLabel {
+    font-family: Arial;
+    font-size: 20px;
+}
+""")
+
+
 
 window.show()
 
