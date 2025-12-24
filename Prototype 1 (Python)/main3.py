844b7cb2f1ff7bfc2702ab654cb669422ce512ea
Dheepan Karthik A <dheepankarthik2008@gmail.com>
2025-12-24 15:40:41 +0530

    Text box successfully configured in main3.py

--- Prototype 1 (Python)/main3.py
+++ Prototype 1 (Python)/main3.py
@@ -1,13 +1,13 @@
 import PySide6
 import sys
-from PySide6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QHBoxLayout, QSizePolicy, QFrame, QLineEdit
+from PySide6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QHBoxLayout, QSizePolicy, QFrame, QLineEdit, QPushButton, QTextEdit, QScrollArea
 from PySide6.QtCore import Qt
 
 app = QApplication(sys.argv)
 
 window = QWidget()
 
-window.setWindowTitle("Document Maker")
+window.setWindowTitle("Document Maker") # Window Title 
 
 window.resize(1600, 900)
 
@@ -21,6 +21,30 @@ workspaceLabel = QLabel("Work Space")
 #   Layouts
 # ==========================
 
+"""
+This part of the program puts in place the following structure of the program:
+
+Main Layout 
+
+-- Title Frame (for the title of the program)
+
+-- Body Frame
+
+-- -- Options (Buttons) Frame
+
+-- -- --  Title of the Options Frame
+
+-- -- --  Section where the buttons are accessible
+
+-- -- Workspace Frame
+
+-- -- -- Title of the Workspace Frame
+
+-- -- -- The actual workspace
+
+
+
+"""
 mainLayout = QVBoxLayout(window)
 
 titleFrame = QFrame()
@@ -72,6 +96,20 @@ workspaceLabel.setAlignment(Qt.AlignTop)
 workspaceLabel.setAlignment(Qt.AlignHCenter)
 
 
+workspaceScrollArea = QScrollArea()
+workspaceScrollArea.setWidgetResizable(True)
+
+workspaceScrollArea.setVerticalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+workspaceScrollArea.setHorizontalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+
+workspaceLayout.addWidget(workspaceScrollArea, stretch=15)
+
+workspaceScrollAreaWidget = QWidget()
+
+workspaceScrollArea.setWidget(workspaceScrollAreaWidget)
+
+workspaceScrollAreaInternalLayout = QVBoxLayout(workspaceScrollAreaWidget)
+
 
 #Styling 
 
@@ -106,6 +144,12 @@ QLabel {
     font-family: Dosis;
     
 }
+                          
+QPushButton {
+                          margin-top: 50px;
+                          margin-bottom: 50px;
+                          padding: 5px;
+}
 """)
 
 workspaceFrame.setStyleSheet("""
@@ -126,6 +170,56 @@ QFrame {
 
 # Child Widgets
 
+## Text Box Button Code & Styling
+
+textBoxButton = QPushButton("Text Box")
+
+buttonLayout.addWidget(textBoxButton)
+
+def addTextBox():
+    global workspaceScrollAreaInternalLayout
+    
+    textBoxToAdd = QTextEdit()
+    textBoxToAdd.setPlaceholderText("Add text")
+    textBoxToAdd.setMinimumHeight(200)
+    textBoxToAdd.setSizePolicy(
+    QSizePolicy.Expanding,
+    QSizePolicy.Fixed 
+    )
+    textBoxToAdd.setStyleSheet(""" QTextEdit {background-color: white} """)
+
+    def deleteTextBox(frame):
+        frame.setParent(None)
+        frame.deleteLater()
+
+    
+    textBoxFrame = QFrame()
+    textBoxLayout = QVBoxLayout(textBoxFrame)
+
+    deleteButton = QPushButton("Delete Text Box")
+    deleteButton.clicked.connect(lambda: deleteTextBox(textBoxFrame))
+    deleteButton.setMaximumHeight(50)
+
+    textBoxLayout.addWidget(textBoxToAdd)
+    textBoxLayout.addWidget(deleteButton)
+
+    
+    workspaceScrollAreaInternalLayout.addWidget(textBoxFrame)
+
+textBoxButton.clicked.connect(addTextBox)
+
+textBoxButton.setStyleSheet("""
+    QPushButton {
+                            background-color: #17BEBB;
+                            color: white;
+    }
+""")
+
+
+## End of Text Box Button & Styling
+
+
+buttonLayout.addStretch()
 
 
 
